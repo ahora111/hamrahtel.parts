3c1e3a9c68b52b531e247f993687e0e27474bcea
ahora111 <nimbuz72@gmail.com>
2025-04-03 15:53:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
+#!/usr/bin/env python3
 import os
 import time
 import requests
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from persiantools.jdatetime import JalaliDate
-
-options = webdriver.ChromeOptions()
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
-
 
 # تنظیمات تلگرام
 BOT_TOKEN = "8187924543:AAH0jZJvZdpq_34um8R_yCyHQvkorxczXNQ"
@@ -23,39 +16,34 @@ CHAT_ID = "-1002683452872"
 # تنظیمات لاگ‌گیری
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-from webdriver_manager.chrome import ChromeDriverManager
-
 def get_driver():
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        service = Service(ChromeDriverManager().install())  # مسیر کروم‌درایور به‌صورت اتوماتیک دریافت می‌شود
+        service = Service()
         driver = webdriver.Chrome(service=service, options=options)
         return driver
     except Exception as e:
         logging.error(f"خطا در ایجاد WebDriver: {e}")
         return None
 
-
 def scroll_page(driver, scroll_pause_time=2):
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
-        driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(scroll_pause_time)
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
 
-
-
 def extract_product_data(driver):
     product_elements = driver.find_elements(By.CLASS_NAME, 'mantine-Text-root')
     models = [product.text.strip().replace("تومانءء", "") for product in product_elements]
     return models[25:]
-    
+
 def is_number(model_str):
     try:
         float(model_str.replace(",", ""))
@@ -72,35 +60,25 @@ def process_model(model_str):
     return model_str
 
 def escape_markdown(text):
-    escape_chars = ['\\', '(', ')', '[', ']', '~', '*', '_', '-', '+', '>', '#', '.', '!', '|', '`', ':']
+    escape_chars = ['\\', '(', ')', '[', ']', '~', '*', '_', '-', '+', '>', '#', '.', '!', '|']
     for char in escape_chars:
-        text = text.replace(char, f'\\{char}')
+        text = text.replace(char, '\\' + char)
     return text
-
-
-test_text = "قیمت گوشی - سامسونگ: 15.900.000 تومان"
-escaped_text = escape_markdown(test_text)
-print(escaped_text)
-
-
-
 
 def split_message(message, max_length=4000):
     return [message[i:i+max_length] for i in range(0, len(message), max_length)]
 
 def send_telegram_message(message, bot_token, chat_id):
-    escaped_message = escape_markdown(message)
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    for part in split_message(escaped_message, 4000):
+    message_parts = split_message(message)
+    for part in message_parts:
+        part = escape_markdown(part)
+        url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
         params = {"chat_id": chat_id, "text": part, "parse_mode": "MarkdownV2"}
         response = requests.get(url, params=params)
-        if not response.json().get('ok'):
+        if response.json().get('ok') is False:
             logging.error(f"❌ خطا در ارسال پیام: {response.json()}")
-        else:
-            logging.info("✅ پیام ارسال شد!")
-
-
-
+            return
+    logging.info("✅ پیام ارسال شد!")
 
 from persiantools.jdatetime import JalaliDate
 
@@ -119,86 +97,53 @@ def create_header(category):
 def create_footer():
     return "\n\n☎️ شماره های تماس :\n📞 09371111558\n📞 02833991417"
 
-
 def categorize_data(models):
-    categorized_data = {"HUAWEI": [], "REDMI_POCO": [], "LCD": [], "OTHER": []}
+    categorized_data = {"HUAWEI": [], "REDMI_POCO": [], "LCD": []}
+    current_key = None
     for model in models:
-        model_upper = model.upper()  # برای راحتی در جستجو، متن را به حروف بزرگ تبدیل می‌کنیم
-        if "HUAWEI" in model_upper:
-            categorized_data["HUAWEI"].append(f"🟥 {model}")
-        elif "REDMI" in model_upper or "POCO" in model_upper or "XIAOMI" in model_upper:
-            categorized_data["REDMI_POCO"].append(f"🟨 {model}")
-        elif "LCD" in model_upper:
-            categorized_data["LCD"].append(f"🟦 {model}")
-        else:
-            categorized_data["OTHER"].append(f"⚪ {model}")  # دسته‌ی سایر برندها
+        if "HUAWEI" in model:
+            current_key = "HUAWEI"
+            categorized_data[current_key].append(f"🟥 {model}")
+        elif "REDMI" in model or "poco" in model:
+            current_key = "REDMI_POCO"
+            categorized_data[current_key].append(f"🟨 {model}")
+        elif "LCD" in model:
+            current_key = "LCD"
+            categorized_data[current_key].append(f"🟦 {model}")
+        elif current_key:
+            categorized_data[current_key].append(model)
     return categorized_data
 
+def send_final_message(bot_token, chat_id):
+    final_message = """
+✅ لیست قطعات گوشیای بالا بروز میباشد. تحویل کالا بعد از ثبت خرید، ساعت 11:30 صبح روز بعد می باشد.
 
-def get_last_post_links(bot_token, chat_id):
-    url = f"https://api.telegram.org/bot{bot_token}/getUpdates?limit=5"
-    response = requests.get(url).json()
+✅ شماره کارت جهت واریز
+🔷 شماره شبا : IR970560611828006154229701
+🔷 شماره کارت : 6219861812467917
+🔷 بلو بانک   حسین گرئی
 
-    samsung_link, xiaomi_link, huawei_link = None, None, None
+⭕️ حتما رسید واریز به ایدی تلگرام زیر ارسال شود.
+🆔 @lhossein1
 
-    if "result" in response:
-        messages = response["result"]
-        for msg in reversed(messages):  # بررسی پیام‌ها از آخر به اول
-            if "message" in msg and "text" in msg["message"]:
-                text = msg["message"]["text"]
-                msg_id = msg["message"]["message_id"]
-
-                if "🟦" in text and samsung_link is None:
-                    samsung_link = f"https://t.me/{chat_id}/{msg_id}"
-                elif "🟨" in text and xiaomi_link is None:
-                    xiaomi_link = f"https://t.me/{chat_id}/{msg_id}"
-                elif "🟥" in text and huawei_link is None:
-                    huawei_link = f"https://t.me/{chat_id}/{msg_id}"
-
-                if samsung_link and xiaomi_link and huawei_link:
-                    break  # اگر هر سه لینک پیدا شدند، متوقف شود
-
-    return samsung_link, xiaomi_link, huawei_link
-
-
-def create_buttons(bot_token, chat_id):
-    samsung_link, xiaomi_link, huawei_link = get_last_post_links(bot_token, chat_id)
-    buttons = []
-    if samsung_link:
-        buttons.append([InlineKeyboardButton("قطعات سامسونگ📱", url=samsung_link)])
-    if xiaomi_link:
-        buttons.append([InlineKeyboardButton("قطعات شیایومی 📱", url=xiaomi_link)])
-    if huawei_link:
-        buttons.append([InlineKeyboardButton("قطعات هوآوی📱", url=huawei_link)])
-    return InlineKeyboardMarkup(buttons)  # هر دکمه را در یک لیست جداگانه قرار می‌دهیم
-
-    
-def send_message_with_buttons(bot_token, chat_id, message):
-    keyboard = create_buttons(bot_token, chat_id)
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    params = {
-        "chat_id": chat_id,
-        "text": message,
-        "parse_mode": "MarkdownV2",
-        "reply_markup": keyboard
-    }
-    response = requests.get(url, params=params)
-    if response.json().get('ok') is False:
-        logging.error(f"❌ خطا در ارسال پیام: {response.json()}")
-        return
-    logging.info("✅ پیام ارسال شد با دکمه‌ها!")
-
+✅ شماره تماس ثبت سفارش:
+📞 09371111558
+📞 02833991417
+"""
+    send_telegram_message(final_message, bot_token, chat_id)
 
 def main():
     try:
         driver = get_driver()
         if not driver:
+            logging.error("❌ نمی‌توان WebDriver را ایجاد کرد.")
             return
         
         driver.get('https://hamrahtel.com/quick-checkout?category=mobile-parts')
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'mantine-Text-root')))
         logging.info("✅ داده‌ها آماده‌ی استخراج هستند!")
         scroll_page(driver)
+
         models = extract_product_data(driver)
         driver.quit()
 
@@ -206,23 +151,12 @@ def main():
             categorized_data = categorize_data(models)
             for category, messages in categorized_data.items():
                 if messages:
-                    header = f"📅 بروزرسانی قیمت: {JalaliDate.today()}\n✅ لیست پخش قطعات {category}\n"
-                    message = header + "\n".join(messages)
-                    send_message_with_buttons(BOT_TOKEN, CHAT_ID, message)
-            
-            final_message = """
-✅ لیست قطعات گوشیای بالا بروز میباشد. تحویل کالا بعد از ثبت خرید، ساعت 11:30 صبح روز بعد می باشد.
-✅ شماره کارت جهت واریز:
-🔷 شماره شبا: IR970560611828006154229701
-🔷 شماره کارت: 6219861812467917
-🔷 بلو بانک: حسین گرئی
-⭕️ رسید واریز به آیدی تلگرام زیر ارسال شود.
-🆔 @lhossein1
-✅ شماره تماس ثبت سفارش:
-📞 09371111558
-📞 02833991417
-            """
-            send_message_with_buttons(BOT_TOKEN, CHAT_ID, final_message)
+                    header = create_header(category)
+                    footer = create_footer()
+                    message = header + "\n".join(messages) + footer
+                    send_telegram_message(message, BOT_TOKEN, CHAT_ID)
+            # ارسال پیام پایانی
+            send_final_message(BOT_TOKEN, CHAT_ID)
         else:
             logging.warning("❌ داده‌ای برای ارسال وجود ندارد!")
     except Exception as e:
